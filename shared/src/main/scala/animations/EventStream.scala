644858b43b65057113b9c019ea329c34a8f1afdb
Teedah Saratoon <teedah.saratoon@gmail.com>
2015-11-03 17:58:12 +0000

    Test app for doodle case study

--- shared/src/main/scala/animations/EventStream.scala
+++ shared/src/main/scala/animations/EventStream.scala
@@ -1,17 +1,25 @@
 package animations
 
+import scala.math
+import doodle.backend.Key
+
 sealed trait EventStream[A] {
   
-  def map[B](f: A => B): EventStream[B]
-    = Map(f)
+  def map[B](f: A => B): EventStream[B] =
+    Map(f)
     
-  def join[B](that: EventStream[B]): EventStream[(A,B)]
-    = Join(this, that)
+  def join[B](that: EventStream[B]): EventStream[(A,B)] =
+    Join(this, that)
   
   def scan[B](seed: B)(f: (A,B) => B): EventStream[B] =
     Scan(seed)(f)
+    
+  def createSource(input: List[Key]): EventStream[A] =
+    Source(input)
   
 }
+import doodle.backend.Key
 final case class Map[A, B](f: A => B) extends EventStream[B]
 final case class Join[A, B](left: EventStream[A], right: EventStream[B]) extends EventStream[(A,B)]
 final case class Scan[A, B](seed: B)(f: (A,B) => B) extends EventStream[B]
+final case class Source[A](input: List[Key]) extends EventStream[A]
---
+++ shared/src/main/scala/animations/TestApp.scala
@@ -0,0 +1,13 @@
+package animations
+
+import doodle.backend.Key._
+
+object TestApp extends App {
+  
+  val input = List(Up, Down, Up, Down, Left, Right)
+  
+  val source = Source(input)
+  
+  
+  
+}
